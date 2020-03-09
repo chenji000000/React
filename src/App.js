8048bc7c904d1fd678f485da175ed0340b977aa3
chenji <chenji000000@163.com>
2020-03-09 15:27:33 +0800

    基本配置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
 import {Button} from 'antd'
+
+import { adminRouter } from "./routes";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Button type='primary'>click</Button>
+        <div>
+          这是公共部分
+        </div>
+        <Switch>
+          {
+            adminRouter.map(route => {
+              return <Route 
+                        key={route.pathname} 
+                        path={route.pathname} 
+                        exact={route.exact}
+                        render={(routerProps) => {
+                          return <route.component {...routerProps} />
+                        }} 
+                      />
+          })
+          }
+          <Redirect to={adminRouter[0].pathname} to="/admin" exact/>
+          <Redirect to="/404" />
+        </Switch>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import App from './App';
 import './index.less'
 
@@ -17,6 +17,8 @@ render(
                     return <Route key={route.pathname} path={route.pathname} component={route.component} />
                 })
             }
+            <Redirect to="/admin" from="/" exact/>
+            <Redirect to="404"/>
         </Switch>
     </Router>,
     document.querySelector("#root")
