7adcb2618264dd6c4cbaf216e46e37f060dd260c
chenji <chenji000000@163.com>
2020-03-09 15:03:21 +0800

    增加路由

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,4 @@
-const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+const { override, fixBabelImports, addLessLoader, addDecoratorsLegacy } = require('customize-cra');
 
 const modifyVars = require('./lessVars')
 
@@ -8,8 +8,9 @@ module.exports = override(
         libraryDirectory: 'es',
         style: true,
     }),
+    addDecoratorsLegacy(),
     addLessLoader({
         javascriptEnabled: true,
         modifyVars,
-    }),
+    }),   
 );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from './App';
 import './index.less'
 
+import { mainRouter } from "./routes";
+
 render(
-    <App />,
+    <Router>
+        <Switch>
+            <Route path="/admin" render={(routerProps) => {
+                return <App {...routerProps} />
+            }}/>
+            {
+                mainRouter.map(route => {
+                    return <Route key={route.pathname} path={route.pathname} component={route.component} />
+                })
+            }
+        </Switch>
+    </Router>,
     document.querySelector("#root")
 )
